1540b12c5c15189c0fabb9248877cfa11c7b0379
@Slayerkkk_ <110862456+slayerkk@users.noreply.github.com>
2023-02-04 18:18:08 -0300

    Add files via upload

--- WIFI.py
+++ WIFI.py
@@ -1,3 +1,3 @@
 import webbrowser as wb
 
-wb.open('https://play.google.com/store/apps/details?id=com.themausoft.wpsapp&hl=pt_BR&gl=US&pli=1')
+wb.open('https://play.google.com/store/apps/details?id=com.themausoft.wpsapp')
